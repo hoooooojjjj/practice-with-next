34b5ecf54b640d755b604a03cb9a15494b1ce625
sono <rhj080471@gmail.com>
2024-05-06 00:34:40 +0900

    emotion 사용해보기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
 "use client";
-import useStore from "./stores/exampleStore";
 
 export default function Home() {
-  const count = useStore().count;
-  return (
-    <div>
-      <div>{count}</div>
-      <button onClick={useStore().increaseCount}>+</button>
-      <button onClick={useStore().decreaseCount}>-</button>
-    </div>
-  );
+  return <div></div>;
 }
--- src/app/username/page.js
+++ src/app/username/page.js
@@ -1,33 +1,20 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import styled from "@emotion/styled";
+const RedButton = styled.button({
+  backgroundColor: "red",
+});
 
+const BlueHeader = styled.h1({
+  color: "blue",
+});
 function page() {
-  async function logJSONData(formdata) {
-    // 사용자 이름을 담은 JSON 객체 생성
-
-    // fetch를 사용하여 서버에 POST 요청 보내기
-    fetch("/username", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formdata),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // 서버로부터 받은 응답을 처리하여 렌더링하기
-        console.log("서버로부터 받은 응답:", data);
-        // 이 부분에서 받은 데이터를 활용하여 원하는 렌더링을 수행할 수 있습니다.
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  }
-
-  useEffect(() => {
-    logJSONData();
-  }, []);
-  return <div></div>;
+  return (
+    <div>
+      <BlueHeader>헤더</BlueHeader>
+      <RedButton>버튼</RedButton>
+    </div>
+  );
 }
 
 export default page;
